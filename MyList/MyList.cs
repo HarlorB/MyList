0955876303c29d1d69a753d182ce4cd09e9f9e81
stb.infoserv <stb.infoserv@gmail.com>
2024-10-02 19:19:17 +0300

    refactoring

---
+++ MyList/MyList.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    class MyList<T>
+    {
+        private T[] _mainArray;
+        public int Length => _mainArray.Length;
+
+        public T this[int index]
+        {
+            get => _mainArray[index];
+            set => _mainArray[index] = value;
+        }
+
+        public MyList()
+        {
+            _mainArray = new T[0];
+
+        }
+
+        public void Add(T value)
+        {
+            T[] MainArrayCopy = new T[_mainArray.Length + 1];
+            for (int i = 0; i < _mainArray.Length; i++)
+            {
+                MainArrayCopy[i] = _mainArray[i];
+            }
+            MainArrayCopy[_mainArray.Length] = value;
+            _mainArray = MainArrayCopy;
+        }
+
+        public void Clear() => _mainArray = new T[0];
+
+        public bool Contains(T value)
+        {
+            bool ContainsOrNot = false;
+            for (int i = 0; i < _mainArray.Length; i++)
+            {
+                if (_mainArray[i].Equals(value))
+                {
+                    ContainsOrNot = true;
+                }
+            }
+            return ContainsOrNot;
+        }
+
+        public int IndexOf(T value)
+        {
+            int IndexNumber = 0;
+            for (int i = 0; i < _mainArray.Length; i++)
+            {
+                if (_mainArray[i].Equals(value))
+                {
+                    IndexNumber = i;
+                }
+            }
+            return IndexNumber;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index >= 0 && index < _mainArray.Length)
+            {
+                T[] MainArrayCopy = new T[_mainArray.Length - 1];
+
+                for (int i = 0; i < index; i++)
+                {
+                    MainArrayCopy[i] = _mainArray[i];
+                }
+
+                for (int i = index + 1; i < _mainArray.Length; i++)
+                {
+                    MainArrayCopy[i - 1] = _mainArray[i];
+                }
+                _mainArray = MainArrayCopy;
+            }
+        }
+
+        public void Remove(T value)
+        {
+            int AmountOfRemovedValues = 0;
+            for (int i = 0; i < _mainArray.Length; i++)
+            {
+                if (_mainArray[i].Equals(value))
+                {
+                    AmountOfRemovedValues += 1;
+                }
+            }
+            int RemovedElementsNum = 0;
+            T[] MainArrayCopy = new T[_mainArray.Length - AmountOfRemovedValues];
+            for (int i = 0; i < _mainArray.Length; i++)
+            {
+                if (_mainArray[i].Equals(value))
+                {
+                    RemovedElementsNum += 1;
+                }
+                else { MainArrayCopy[i - RemovedElementsNum] = _mainArray[i]; }
+            }
+            _mainArray = MainArrayCopy;
+        }
+
+        public void Insert(int index, T value)
+        {
+            if (index >= 0 && index < _mainArray.Length)
+            {
+                T[] MainArrayCopy = new T[_mainArray.Length + 1];
+                for (int i = 0; i < index; i++)
+                {
+                    MainArrayCopy[i] = _mainArray[i];
+                }
+                MainArrayCopy[index] = value;
+                for (int i = index + 1; i < _mainArray.Length + 1; i++)
+                {
+                    MainArrayCopy[i] = _mainArray[i - 1];
+                }
+                _mainArray = MainArrayCopy;
+            }
+        }
+
+        public void ShowArray()
+        {
+            for (int i = 0; i < _mainArray.Length; i++)
+            {
+                Console.Write(_mainArray[i] + " ");
+            }
+            Console.Write("\n");
+        }
+    }
+}
--- MyList/Program.cs
+++ MyList/Program.cs
@@ -1,153 +1,16 @@
-﻿using System;
-using System.Collections;
-using System.ComponentModel;
-using System.Numerics;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-
-namespace MyList
+﻿namespace MyList
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            MyList list = new MyList();
+            MyList<int> list = new MyList<int>();
             list.Add(1);
-            list.Add("aaa");
             list.Add(1);
-            list.Insert(1,false);
+            list.Insert(1,111);
             Console.WriteLine(list[0]);
-
+            Console.WriteLine(list.Length);
         }
-    }
-    class MyList
-    {
-        private System.Object[] _MainArray = new System.Object[0];
-        public int lenght = _MainArray.Length;
-
-    public System.Object this[int index]
-        {
-            get => _MainArray[index];
-            set => _MainArray[index] = value;
-        }
-
-        public void Add(System.Object value)
-        {
-            System.Object[] MainArrayCopy = new System.Object[_MainArray.Length + 1];
-            for (int i = 0; i < _MainArray.Length; i++)
-            {
-                MainArrayCopy[i] = _MainArray[i];
-            }
-            MainArrayCopy[_MainArray.Length] = value;
-            _MainArray = MainArrayCopy;
-        }
-
-        public void Clear()
-        {
-            _MainArray = new System.Object[0];
-        }
-
-        public bool Contains(System.Object value)
-        {
-            bool ContainsOrNot = false;
-            for (int i = 0; i < _MainArray.Length; i++)
-            {
-                if (_MainArray[i].Equals(value))
-                {
-                    ContainsOrNot = true;
-                }
-            }
-            return ContainsOrNot;
-        }
-
-        public int IndexOf(System.Object value)
-        {
-            int IndexNumber = 0;
-            for (int i = 0; i < _MainArray.Length; i++)
-            {
-                if (_MainArray[i].Equals(value))
-                {
-                    IndexNumber = i;
-                }
-            }
-            return IndexNumber;
-        }
-
-        public void RemoveAt(int index)
-        {
-            if (index >= 0 && index < _MainArray.Length)
-            {
-                System.Object[] MainArrayCopy = new System.Object[_MainArray.Length - 1];
-
-                for (int i = 0; i < index; i++)
-                {
-                    MainArrayCopy[i] = _MainArray[i];
-                }
-
-                for (int i = index + 1; i < _MainArray.Length; i++)
-                {
-                    MainArrayCopy[i - 1] = _MainArray[i];
-                }
-                _MainArray = MainArrayCopy;
-            }
-        }
-
-        public void Remove(System.Object value)
-        {
-            int AmountOfRemovedValues = 0;
-            for (int i = 0; i < _MainArray.Length; i++)
-            {
-                if (_MainArray[i].Equals(value))
-                {
-                    AmountOfRemovedValues += 1;
-                }
-            }
-            int RemovedElementsNum = 0;
-            System.Object[] MainArrayCopy = new System.Object[_MainArray.Length - AmountOfRemovedValues];
-            for (int i = 0; i < _MainArray.Length; i++)
-            {
-                if (_MainArray[i].Equals(value))
-                {
-                    RemovedElementsNum += 1;
-                }
-                else { MainArrayCopy[i - RemovedElementsNum] = _MainArray[i]; }
-            }
-            _MainArray = MainArrayCopy;
-        }
-
-        public void Insert(int index, System.Object value)
-        {
-            if (index >= 0 && index < _MainArray.Length)
-            {
-                System.Object[] MainArrayCopy = new System.Object[_MainArray.Length + 1];
-                for (int i = 0; i < index; i++)
-                {
-                    MainArrayCopy[i] = _MainArray[i];
-                }
-                MainArrayCopy[index] = value;
-                for (int i = index + 1; i < _MainArray.Length + 1; i++)
-                {
-                    MainArrayCopy[i] = _MainArray[i - 1];
-                }
-                _MainArray = MainArrayCopy;
-            }
-        }
-
-        public int Lenght()
-        {
-            return _MainArray.Length;
-        }
-
-        public void ShowArray()
-        {
-            for (int i = 0; i < _MainArray.Length; i++)
-            {
-                Console.Write(_MainArray[i] + " ");
-            }
-            Console.Write("\n");
-        }
-
-
-
     }
 }
 
